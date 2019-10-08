d0883c73899490ba2fc073d562d32e696e44265a
Rafael Barros <rafaelbarros.df@gmail.com>
2019-10-07 23:20:23 -0300

    :construction: Adicionando a Página as abas

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FeedPageModule } from './../pages/feed/feed.module';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FeedPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
