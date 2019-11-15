71098cd6bb7d274a3786c5e14c21e5355e4fc4c1
Rafael Barros <rafaelbarros.df@gmail.com>
2019-11-15 12:51:59 -0300

    :construction: Exibir mensagem enquanto a lista de filmes é carregada

--- myapp/src/pages/feed/feed.ts
+++ myapp/src/pages/feed/feed.ts
@@ -1,6 +1,6 @@
 import { MovieProvider } from './../../providers/movie/movie';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the FeedPage page.
@@ -32,22 +32,43 @@ export class FeedPage {
 
   public listaFilmes = new Array<any>();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private movieProvider: MovieProvider) {
+  public loader;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private movieProvider: MovieProvider, 
+    public loadingCtrl: LoadingController) {
   }
+
+  abreCarregando() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes..."
+    });
+    this.loader.present();
+  }
+
+  fechaCarregando() {
+    this.loader.dismiss();
+  }
+
+
 
   public somaDoisNumeros(num1: number, num2: number): void {
      alert(num1 + num2)
   }
 
-  ionViewDidLoad() {
-    // this.somaDoisNumeros(10, 99);
-    console.log('ionViewDidLoad FeedPage');
+  ionViewDidEnter() {
+    this.abreCarregando();
+    console.log('ionViewDidEnter FeedPage');
 
     this.movieProvider.getPopularMovies().subscribe(resp => {
       this.listaFilmes = resp['results'];
+      this.fechaCarregando();
     }, error => {
       console.error(error);
-    })
+    });
+
   }
 
 }
