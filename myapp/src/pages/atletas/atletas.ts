1951f7b49d4d172bfdf75cc9a8550ec4f4aee23c
Rafael Barros <rafaelbarros.df@gmail.com>
2019-11-15 20:43:41 -0300

    :construction: Usando o plugin nativo da Câmera

--- myapp/src/pages/atletas/atletas.ts
+++ myapp/src/pages/atletas/atletas.ts
@@ -1,6 +1,7 @@
 import { CartolaProvider } from './../../providers/cartola/cartola';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the AtletasPage page.
@@ -13,14 +14,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-atletas',
   templateUrl: 'atletas.html',
-  providers: [CartolaProvider]
+  providers: [
+    CartolaProvider,
+    Camera
+  ]
 })
 export class AtletasPage {
+
+  img = '';
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private _cartolaProvider: CartolaProvider) {
+    private _cartolaProvider: CartolaProvider,
+    private camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -28,6 +35,22 @@ export class AtletasPage {
       console.log(resp);
     }, err => {
       console.error('error: ', err);
+    });
+  }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     this.img = 'data:image/jpeg;base64,' + imageData;
+     console.log(this.img);
+    }, (err) => {
+     // Handle error
     });
   }
 
